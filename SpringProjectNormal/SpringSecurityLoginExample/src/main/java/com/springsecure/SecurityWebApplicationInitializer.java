e588dac931f2a24330e236eab2900c5894ff9fcb
jain1197 <anant.jain@timesinternet.in>
2021-08-24 11:43:36 +0530

    login example

---
+++ SpringProjectNormal/SpringSecurityLoginExample/src/main/java/com/springsecure/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.springsecure;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
